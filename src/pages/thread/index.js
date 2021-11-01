pullpush-io/undelete

commit 20eae33207f8dba89918cfee40e964f5578e9dbb

Use Reddit for comments with missing parents
Pushshift occasionally fails to return comments which are the parents
of comments that it does return. Look these parents up in the Reddit
API and use its results instead, otherwise entire comment trees may be
hidden. They could still be hidden if two consecutive parents are
missing from Pushshift, but hopefully that isn't a common occurance.

src/pages/thread/index.js
@@ -51,40 +51,45 @@ class Thread extends React.Component {
     // Get comment ids from pushshift
     getPushshiftComments(threadID)
       .then(pushshiftComments => {
+        console.log(`Pushshift: ${pushshiftComments.length} comments`)
+        const pushshiftCommentLookup = new Map(pushshiftComments.map(c => [c.id, c]))
+        const ids = []
+
         // Extract ids from pushshift response
-        const ids = pushshiftComments.map(comment => comment.id)
-        this.props.global.setLoading('Comparing comments to Reddit API...')
+        pushshiftComments.forEach(comment => {
+          ids.push(comment.id)
+          if (comment.parent_id != threadID && !pushshiftCommentLookup.has(comment.parent_id)) {
+            ids.push(comment.parent_id)
+          }
+        });
+
         // Get all the comments from reddit
+        this.props.global.setLoading('Comparing comments to Reddit API...')
         return getRedditComments(ids)
           .then(redditComments => {
-            // Temporary lookup for updating score
-            const redditCommentLookup = {}
-            redditComments.forEach(comment => {
-              redditCommentLookup[comment.id] = comment
-            })
-
-            // Replace pushshift score with reddit (its usually more accurate)
-            pushshiftComments.forEach(comment => {
-              const redditComment = redditCommentLookup[comment.id]
-              if (redditComment !== undefined) {
-                comment.score = redditComment.score
-              }
-            })
-
+            console.log(`Reddit: ${redditComments.length} comments`)
             const removed = []
             const deleted = []
 
-            // Check what as removed / deleted according to reddit
             redditComments.forEach(comment => {
+              const pushshiftComment = pushshiftCommentLookup.get(comment.id)
+              if (pushshiftComment === undefined) {
+                // When a parent comment is missing from pushshift, use the reddit comment instead
+                comment.parent_id = comment.parent_id.substring(3)
+                comment.link_id = comment.link_id.substring(3)
+                pushshiftComments.push(comment)
+              } else {
+                // Replace pushshift score with reddit (it's usually more accurate)
+                pushshiftComment.score = comment.score
+              }
+
+              // Check what is removed / deleted according to reddit
               if (isRemoved(comment.body)) {
                 removed.push(comment.id)
               } else if (isDeleted(comment.body)) {
                 deleted.push(comment.id)
               }
             })
-
-            console.log(`Pushshift: ${pushshiftComments.length} comments`)
-            console.log(`Reddit: ${redditComments.length} comments`)
 
             this.props.global.setSuccess()
             this.setState({

commit 47b4d913bd6f75dcb67c4f23f0377a6dce5acfe0

Fix small bug introduced in 20eae33
Duplicate comments were being retrieved from Reddit under certain
uncommon circumstances, this also caused the comment counts to be off

src/pages/thread/index.js
@@ -54,14 +54,19 @@ class Thread extends React.Component {
         console.log(`Pushshift: ${pushshiftComments.length} comments`)
         const pushshiftCommentLookup = new Map(pushshiftComments.map(c => [c.id, c]))
         const ids = []
+        const missingIds = new Set()
 
         // Extract ids from pushshift response
         pushshiftComments.forEach(comment => {
           ids.push(comment.id)
-          if (comment.parent_id != threadID && !pushshiftCommentLookup.has(comment.parent_id)) {
+          if (comment.parent_id != threadID &&
+              !pushshiftCommentLookup.has(comment.parent_id) &&
+              !missingIds.has(comment.parent_id)) {
             ids.push(comment.parent_id)
+            missingIds.add(comment.parent_id)
           }
         });
+        missingIds.clear()
 
         // Get all the comments from reddit
         this.props.global.setLoading('Comparing comments to Reddit API...')