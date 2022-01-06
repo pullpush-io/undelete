3230ebc0d8a0285f9ec4d47eb9d964937a8db1bc
Christopher Gurnee <chris@gurneeconsulting.net>
2022-01-06 13:09:08 -0500

    Stop using OAuth2 with the Reddit API
    None of the API endpoints being used require authentication,
    its use was slowing things down a bit for no advantage.

--- src/api/reddit/index.js
+++ src/api/reddit/index.js
@@ -1,5 +1,6 @@
 import { fetchJson, chunk } from '../../utils'
-import { getAuth } from './auth'
+
+const baseURL = 'https://api.reddit.com'
 
 const errorHandler = (error, from) => {
   console.error(from + ': ' + error)
@@ -8,32 +9,26 @@ const errorHandler = (error, from) => {
 
 // Return the post itself
 export const getPost = (subreddit, threadID) => (
-  getAuth()
-    .then(auth => fetchJson(`https://oauth.reddit.com/comments/${threadID}.json?limit=1`, auth))
+  fetchJson(`${baseURL}/comments/${threadID}.json?limit=1`)
     .then(thread => thread[0].data.children[0].data)
     .catch(error => errorHandler(error, 'reddit.getPost'))
 )
 
 //// Fetch multiple threads (via the info endpoint)
-//export const getThreads = threadIDs => {
-//  return getAuth()
-//    .then(auth => fetchJson(`https://oauth.reddit.com/api/info?id=${threadIDs.map(id => `t3_${id}`).join()}`, auth))
+//export const getThreads = threadIDs => (
+//  fetchJson(`${baseURL}/api/info?id=${threadIDs.map(id => `t3_${id}`).join()}`)
 //    .then(response => response.data.children.map(threadData => threadData.data))
 //    .catch(error => errorHandler(error, 'reddit.getThreads'))
-//}
+//)
 
 // Helper function that fetches a list of comments
-const fetchComments = (commentIDs, auth) => {
-  return fetchJson(`https://oauth.reddit.com/api/info?id=${commentIDs.map(id => `t1_${id}`).join()}`, auth)
+const fetchComments = (commentIDs) => (
+  fetchJson(`${baseURL}/api/info?id=${commentIDs.map(id => `t1_${id}`).join()}`)
     .then(results => results.data.children.map(({data}) => data))
-}
+)
 
-export const getComments = commentIDs => {
-  return getAuth()
-    .then(auth => (
-      Promise.all(chunk(commentIDs, 100)
-        .map(ids => fetchComments(ids, auth)))
-        .then(results => results.flat())
-    ))
+export const getComments = commentIDs => (
+  Promise.all(chunk(commentIDs, 100).map(ids => fetchComments(ids)))
+    .then(results => results.flat())
     .catch(error => errorHandler(error, 'reddit.getComments'))
-}
+)
