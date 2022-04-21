pullpush-io/undelete

commit 536bccba2a86cc5b49ac69a063fffd2a589bdcbd

Fix visits to full posts but with a bookmark/hash
Beforehand, only initial visits to single comments (permalinks)
and internal links correctly scrolled to the bookmark/hash.

src/pages/thread/index.js
@@ -230,8 +230,10 @@ class Thread extends React.Component {
       // Set the scroll location to just below the post if not already set (only with permalinks)
       if (!this.props.location.hash)
         this.props.location.hash = '#comment-info'
-      this.props.location.state = {scrollBehavior: 'smooth'}
     }
+
+    if (this.props.location.hash)
+      this.props.location.state = {scrollBehavior: 'smooth'}
   }
 
   // Updates this.curContigIdx based on URL's commentID if it's already downloaded.
@@ -337,12 +339,13 @@ class Thread extends React.Component {
       }
     }
 
-    if (!loadingComments && !this.props.global.isErrored() && this.props.location.state?.scrollBehavior) {
-      const { location } = this.props
-      const id = location.hash.substring(1)
-      if (id)
-        document.getElementById(id)?.scrollIntoView({behavior: location.state.scrollBehavior})
-      delete location.state
+    const { location } = this.props
+    if (location.state?.scrollBehavior && location.hash.length > 1 && !this.props.global.isErrored()) {
+      const hashElem = document.getElementById(location.hash.substring(1))
+      if (hashElem) {
+        hashElem.scrollIntoView({behavior: location.state.scrollBehavior})
+        delete location.state
+      }
     }
 
     if (this.nextAllCommentsFiltered != this.state.allCommentsFiltered)

commit 6d687e188cbb414ece2e5b197f56f907dae3ac6c

Fix default of scrolling past the post w/permalinks
Regression from 536bccb

src/pages/thread/index.js
@@ -318,8 +318,7 @@ class Thread extends React.Component {
                 this.setCurContig(insertBefore - 1)  // (this was the failed earlier attempt)
                 console.timeEnd('Load comments')
                 this.props.global.setSuccess()
-                loadingComments = false
-                this.setState({pushshiftCommentLookup, loadingComments, reloadingComments: false})
+                this.setState({pushshiftCommentLookup, loadingComments: false, reloadingComments: false})
               }
             } else
               createdUtcNotFound = true
@@ -341,7 +340,8 @@ class Thread extends React.Component {
     }
 
     const { location } = this.props
-    if (location.state?.scrollBehavior && location.hash.length > 1 && !this.props.global.isErrored()) {
+    if (location.state?.scrollBehavior && location.hash.length > 1 &&
+        !loadingComments && !this.props.global.isErrored()) {
       const hashElem = document.getElementById(location.hash.substring(1))
       if (hashElem) {
         hashElem.scrollIntoView({behavior: location.state.scrollBehavior})